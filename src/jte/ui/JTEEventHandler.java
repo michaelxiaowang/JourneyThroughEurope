b3c2502eb53259b7a5a561bc7f57a3f4e4a09916
michaelxiaowang <michaelxiaowang@gmail.com>
2014-11-08 17:02:31 -0500

    Made basic splash screen, added play, load, about and exit game buttons. Implemented exit game event handler.

--- src/jte/ui/JTEEventHandler.java
+++ src/jte/ui/JTEEventHandler.java
@@ -6,6 +6,18 @@
 
 package jte.ui;
 
+import application.Main.JTEPropertyType;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import properties_manager.PropertiesManager;
+
 /**
  *
  * @author Michael
@@ -16,5 +28,51 @@ public class JTEEventHandler {
     
     public JTEEventHandler(JTEUI initUI) {
         ui = initUI;
+    }
+    
+    //Exit Button
+    public void respondToExitRequest(Stage primaryStage) {
+        // ENGLIS IS THE DEFAULT
+        String options[] = new String[]{"Yes", "No"};
+        PropertiesManager props = PropertiesManager.getPropertiesManager();
+        options[0] = props.getProperty(JTEPropertyType.DEFAULT_YES_TEXT);
+        options[1] = props.getProperty(JTEPropertyType.DEFAULT_NO_TEXT);
+        String verifyExit = props.getProperty(JTEPropertyType.DEFAULT_EXIT_TEXT);
+
+        // NOW WE'LL CHECK TO SEE IF LANGUAGE SPECIFIC VALUES HAVE BEEN SET
+        if (props.getProperty(JTEPropertyType.YES_TEXT) != null) {
+            options[0] = props.getProperty(JTEPropertyType.YES_TEXT);
+            options[1] = props.getProperty(JTEPropertyType.NO_TEXT);
+            verifyExit = props.getProperty(JTEPropertyType.EXIT_REQUEST_TEXT);
+        }
+
+        // FIRST MAKE SURE THE USER REALLY WANTS TO EXIT
+        Stage dialogStage = new Stage();
+        dialogStage.initModality(Modality.WINDOW_MODAL);
+        dialogStage.initOwner(primaryStage);
+        BorderPane exitPane = new BorderPane();
+        HBox optionPane = new HBox();
+        optionPane.setAlignment(Pos.CENTER);
+        optionPane.setPadding(new Insets(10, 10, 10, 10));
+        Button yesButton = new Button(options[0]);
+        Button noButton = new Button(options[1]);
+        optionPane.setSpacing(10.0);
+        optionPane.getChildren().addAll(yesButton, noButton);
+        Label exitLabel = new Label(verifyExit);
+        exitPane.setCenter(exitLabel);
+        exitPane.setBottom(optionPane);
+        Scene scene = new Scene(exitPane, 200, 150);
+        dialogStage.setScene(scene);
+        dialogStage.show();
+        // WHAT'S THE USER'S DECISION?
+        yesButton.setOnAction(e -> {
+            // YES, LET'S EXIT
+            System.exit(0);
+        });
+        noButton.setOnAction(e -> {
+           // NO, LET'S NOT
+            dialogStage.close();
+        });
+
     }
 }
--- src/jte/ui/JTEUI.java
+++ src/jte/ui/JTEUI.java
@@ -7,9 +7,18 @@
 package jte.ui;
 
 import application.Main.JTEPropertyType;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import jte.game.JTEGameData;
 import jte.game.JTEGameStateManager;
@@ -20,6 +29,11 @@ import properties_manager.PropertiesManager;
  * @author Michael
  */
 public class JTEUI extends Pane{
+    //PropertiesManager
+    PropertiesManager props = PropertiesManager.getPropertiesManager();
+    
+    // Image path
+    private String ImgPath = "file:images/";
     
     // mainStage
     private Stage primaryStage;
@@ -28,6 +42,13 @@ public class JTEUI extends Pane{
     private BorderPane mainPane;
     private int paneWidth;
     private int paneHeigth;
+    
+    //splashScreen
+    private VBox splashPane;
+    private HBox menuBox;
+    private Image splashImage;
+    private ImageView splashImageView;
+    private Label splashImageLabel;
     
     // Padding
     private Insets marginlessInsets;
@@ -38,11 +59,14 @@ public class JTEUI extends Pane{
     //GameStateManager
     JTEGameStateManager gsm;
     
+    /**
+     * Constructor creates JTEEventHandler, and GameStateManager
+     */
     public JTEUI() {
         gsm = new JTEGameStateManager(this);
         eventHandler = new JTEEventHandler(this);
         initMainPane();
-        //initSplashScreen();
+        initSplashScreen();
     }
     
     public void SetStage(Stage stage) {
@@ -66,12 +90,69 @@ public class JTEUI extends Pane{
         marginlessInsets = new Insets(5, 5, 5, 5);
         mainPane = new BorderPane();
 
-        PropertiesManager props = PropertiesManager.getPropertiesManager();
         paneWidth = Integer.parseInt(props
                 .getProperty(JTEPropertyType.WINDOW_WIDTH));
         paneHeigth = Integer.parseInt(props
                 .getProperty(JTEPropertyType.WINDOW_HEIGHT));
         mainPane.resize(paneWidth, paneHeigth);
         mainPane.setPadding(marginlessInsets);
+    }
+    
+    public void initSplashScreen() {
+        menuBox = new HBox();
+        splashPane = new VBox();
+        menuBox.setAlignment(Pos.CENTER);
+        menuBox.setSpacing(10.0);
+        splashImage = loadImage(props.getProperty(JTEPropertyType.SPLASH_SCREEN_IMAGE_NAME));
+        splashImageView = new ImageView(splashImage);
+        splashImageLabel = new Label();
+        splashImageLabel.setGraphic(splashImageView);
+        splashPane.getChildren().add(splashImageLabel);
+        
+        //Make the play button
+        Image playButtonImage = loadImage(props.getProperty(JTEPropertyType.PLAY_IMG_NAME));
+        ImageView playButtonView = new ImageView(playButtonImage);
+        Button playButton = new Button();
+        playButton.setGraphic(playButtonView);
+        
+        //Make the load button
+        Image loadButtonImage = loadImage(props.getProperty(JTEPropertyType.LOAD_IMG_NAME));
+        ImageView loadButtonView = new ImageView(loadButtonImage);
+        Button loadButton = new Button();
+        loadButton.setGraphic(loadButtonView);
+        loadButton.setDisable(true);
+        
+        //Make the about button
+        Image aboutButtonImage = loadImage(props.getProperty(JTEPropertyType.ABOUT_IMG_NAME));
+        ImageView aboutButtonView = new ImageView(aboutButtonImage);
+        Button aboutButton = new Button();
+        aboutButton.setGraphic(aboutButtonView);
+        
+        //Make the exit button
+        Image exitButtonImage = loadImage(props.getProperty(JTEPropertyType.EXIT_IMG_NAME));
+        ImageView exitButtonView = new ImageView(exitButtonImage);
+        Button exitButton = new Button();
+        exitButton.setGraphic(exitButtonView);
+        exitButton.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                // TODO Auto-generated method stub
+                eventHandler.respondToExitRequest(primaryStage);
+            }
+
+        });
+        
+        //Add the buttons
+        menuBox.getChildren().addAll(playButton, loadButton, aboutButton, exitButton);
+        
+        //Set the mainPane to splashScreen components
+        mainPane.setCenter(splashImageView);
+        mainPane.setBottom(menuBox);
+    }
+    
+    public Image loadImage(String imageName) {
+        Image img = new Image(ImgPath + imageName);
+        return img;
     }
 }
